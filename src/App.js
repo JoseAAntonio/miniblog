2cf5d6f455598c038e80481b005aa97f757e53d8
Jose Antonio <antoniojaraujo0@gmail.com>
2023-03-12 13:05:41 -0300

    setting up routes with react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+//pages
+import {Home} from './pages/home/Home';
+import {About} from './pages/about/About';
+
+
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>miniBlog</h1>
+      <BrowserRouter>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
